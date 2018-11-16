012f7755f8a6ede40ffdef885b656adc90f6c9d3
Jason Walsh <jwalsh@saylent.com>
2018-11-16 17:33:18 -0500

    Standardize data sources in S3

--- s3-upload.sh
+++ s3-upload.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-aws s3 cp . s3://aws-ml-bootcamp/ --recursive  --exclude "*" --include "*.csv"
+BUCKET=aws-ml-bootcamp
+aws s3api create-bucket --bucket $BUCKET --region us-east-1
+
+aws s3 cp . s3://$BUCKET/ --recursive  --exclude "*" --include "*.csv"
