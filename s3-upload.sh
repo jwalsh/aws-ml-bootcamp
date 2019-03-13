38b3027ae025b2e5ff7f32221e85b55922653199
Jason Walsh <jwalsh@saylent.com>
2019-03-13 16:50:20 -0400

    Allow training and test data to be pulled without manual upload

--- s3-upload.sh
+++ s3-upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-BUCKET=aws-ml-bootcamp
+# Making the bucket public
+BUCKET=aws-ml-bootcamp-jwalsh
 aws s3api create-bucket --bucket $BUCKET --region us-east-1
 
 aws s3 cp . s3://$BUCKET/ --recursive  --exclude "*" --include "*.csv"
